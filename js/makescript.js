9c96cd15a25110ffe5a5b9d9b9cc058a24e27f0d
Sergei Trush <sergei.trush89@gmail.com>
2016-05-28 13:54:23 +0300

    no media

---
+++ js/makescript.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$(".slider-img").bxSlider({
+		captions: true,
+		controls: false,
+		auto: true,
+		speed: 3000,
+		pagerCustom: '.slider-pager',
+		mode: 'fade',
+	})
+});
+
+
+
+
+
+
+
